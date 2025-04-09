0bcbec65f90c2f002f2f3e4d7b0505178f6d8bb3
Chadrick <chadrickalvarez@gmail.com>
2025-04-09 10:38:27 -0400

    Lab Using Statements

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,22 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+if (temperature > 80) {
+    console.log('Watering on');
+} else console.log('Watering off');
+
+if (timeOfDay === "evening") {
+    console.log('Lights on')
+     if (timeOfDay === 'night')
+    console.log('Lights on')
+} else (timeOfDay === 'morning')
+console.log('Lights off');
+
+
+while (soilMoisture < 40) {
+    console.log('Soil moisture is ' + soilMoisture);
+    soilMoisture +=5;
+}
+for (let i = 5; i <= 40; i+=5) 
+    console.log(i);
